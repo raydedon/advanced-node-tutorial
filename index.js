7997628f915896e5c128ef001bcca3faa5032902
raydedon <raydedon@gmail.com>
2018-11-04 21:29:46 +0530

    cluster enhancement

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const createError = require('http-errors');
 const express = require('express');
-const app = express();
 const url = require('url');
 const morgan = require('morgan');
 const path = require('path');
@@ -8,7 +7,9 @@ const ejs = require('ejs');
 const cookieParser = require('cookie-parser');
 const config = require('config');
 const mongoose = require('mongoose');
-
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 const indexRouter = require('./routes/indexRoute');
 const loginRouter = require('./routes/loginRoute');
 const contactsRouter = require('./routes/contactsRoute');
@@ -25,39 +26,62 @@ mongoose.connect(config.get('MongoDB.connectionString'))
 		process.exit();
 	});
 
+if (cluster.isMaster && config.get('App.isCluster')) {
+	console.log(`Master ${process.pid} is running`);
+
+	// Fork workers.
+	for (let i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('online', function(worker) {
+		console.log(`Worker ${worker.process.pid} is online`);
+	});
+
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+		console.log('Starting a new worker');
+		cluster.fork();
+	});
+} else {
+	const app = express();
+
+	let myLogger = (req, res, next) => {
+		console.log('LOGGED');
+		next();
+	};
+
+	let requestTime = (req, res, next) => {
+		req.requestTime = Date.now();
+		next();
+	};
 
 
-let myLogger = (req, res, next) => {
-	console.log('LOGGED');
-	next();
-};
 
-let requestTime = (req, res, next) => {
-	req.requestTime = Date.now();
-	next();
-};
+	app.use(morgan('short'));
+	app.use(myLogger);
+	app.use(requestTime);
+	app.use(express.json());
+	app.use(express.urlencoded({extended: false}));
+	app.use(cookieParser());
 
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'ejs');
 
+	app.use('/', indexRouter);
+	app.use('/login', loginRouter);
+	app.use('/contact', contactsRouter);
 
-app.use(morgan('short'));
-app.use(myLogger);
-app.use(requestTime);
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
+	app.use((req, res) => {
+		res.status(404).send('Page not found. Try another.');
+	});
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+	let mode = process.env.NODE_ENV || 'development';
 
-app.use('/', indexRouter);
-app.use('/login', loginRouter);
-app.use('/contact', contactsRouter);
-
-app.use((req, res) => {
-	res.status(404).send('Page not found. Try another.');
-});
-
-app.listen(3000, (error) => {
-	console.log('Server listening on port');
-});
+	app.listen(config.get('App.webServer.port'), (error) => {
+		console.log(`Process ${process.pid} is listening to all incoming requests 
+		mode: ${mode} 
+		Server listening on port: ${config.get('App.webServer.port')}`);
+	});
+}
 

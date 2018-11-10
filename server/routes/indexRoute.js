c5cb41d804d5d3fb88c6a13a34efe805c69ea8ba
raydedon <raydedon@gmail.com>
2018-11-10 18:04:57 +0530

    login completed with passport

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const passport = require('passport');
+const gravatar = require('gravatar');
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res, next) => {
+indexRouter.get('/', preventNextIfLoggedIn, (req, res, next) => {
+	console.info(`session: ${JSON.stringify(req.session, null, 4)}`);
 	res.render('index', {
 		title: 'Contacts'
 	});
 });
 
 /* GET login page. */
-indexRouter.get('/login', function(req, res, next) {
+indexRouter.get('/login', preventNextIfLoggedIn, function(req, res, next) {
+	console.info(`session: ${JSON.stringify(req.session, null, 4)}`);
 	res.render('login', { title: 'Login', message: req.flash('loginMessage') });
 });
 
-indexRouter.get('/signup', function(req, res, next) {
+indexRouter.post('/login', passport.authenticate('local-login', {
+	//Success go to Profile Page / Fail go to login page
+	successRedirect : '/profile',
+	failureRedirect : '/login',
+	failureFlash : true
+}));
+
+indexRouter.get('/signup', preventNextIfLoggedIn, function(req, res, next) {
+	console.info(`session: ${JSON.stringify(req.session, null, 4)}`);
 	res.render('signup', { title: 'SignUp', message: req.flash('signupMessage') });
 });
 
@@ -26,13 +37,33 @@ indexRouter.post('/signup', passport.authenticate('local-signup', {
 }));
 
 
-indexRouter.get('/profile', function(req, res, next) {
-	res.render('profile', { title: 'Profile', message: 'View profiles' });
+indexRouter.get('/profile', isLoggedIn, function(req, res, next) {
+	console.info(`session: ${JSON.stringify(req.session, null, 4)}`);
+	res.render('profile', { title: 'Profile', message: 'View profiles', user : req.user, avatar: gravatar.url(req.user.email ,  {s: '100', r: 'x', d: 'retro'}, true) });
 });
 
 indexRouter.get('/comments', function(req, res, next) {
 	res.render('comments', { title: 'Comments', message: 'Please put your comments below' });
 });
 
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+	res.redirect('/login');
+}
+
+function preventNextIfLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		res.redirect('/profile');
+	next();
+}
+
+/* GET Logout Page */
+indexRouter.get('/logout', function(req, res) {
+	req.session.destroy(function(err) {
+		req.logout();
+		res.redirect('/');
+	});
+});
 
 exports = module.exports = indexRouter;
